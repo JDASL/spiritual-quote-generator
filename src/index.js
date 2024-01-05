a33a1976b3ba0e870c091221cddcd14da9d273ae
JDASL <127839733+JDASL@users.noreply.github.com>
2024-01-05 12:44:24 +0100

    added basic form handling, typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert(Welcome);
+function generateQuote(event) {
+  event.preventDefault();
+
+  new Typewriter("#quote", {
+    strings: ["The way is not in the sky; the way is in the heart. – Buddha"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let quoteElement = document.querySelector("#quote-generator-form");
+quoteElement.addEventListener("submit", generateQuote);
